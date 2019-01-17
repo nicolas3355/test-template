edda771fccf8d7980fb745dc0b04f84602d2f257
nicolas3355 <nicolaselhaddad.nh@gmail.com>
2019-01-17 11:50:34 -0500

    added a simple test for an increment function

---
+++ test0.py
@@ -0,0 +1,3 @@
+def inc_test(x):
+    # put your code here
+    # return x + 1
---
+++ test_sample.py
@@ -0,0 +1,6 @@
+# content of test_sample.py
+from test0 import inc_test
+
+
+def test_answer():
+    assert inc_test(3) == 4
